bd87de85e19aa88e5a14ee93a15ea47153132f42
Eugene Bochkov <ebochkov@playtika.com.ua>
2015-02-09 13:22:01 +0200

    Fixed plot4.R

--- plot4.R
+++ plot4.R
@@ -1,9 +1,5 @@
-# data = read.table('working-data.txt')
-# png(filename = "plot4.png")
-# 
-# plot()
-# 
-# dev.off()
+data = read.table('working-data.txt')
+png(filename = "plot4.png")
 
 par(mfrow = c(2,2))
 
@@ -17,4 +13,6 @@ lines(x, data$Sub_metering_2, col = "red")
 lines(x, data$Sub_metering_3, col = "blue")
 legend('topright', c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lty = 1, col = c('black', 'red', 'blue'), bty = 'n')
 
-plot(data$Global_reactive_power, type="l", ylab='Voltage', xlab='datetime')
+plot(data$Global_reactive_power, type="l", ylab='Global_reactive_power', xlab='datetime')
+
+dev.off()
